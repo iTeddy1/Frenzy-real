f27ab1c530e470638f455554dd8ba42118d10e16
iTeddy1 <duytrung.ng1@gmail.com>
2024-05-14 22:39:44 +0700

    Refactor form component

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,39 +1,3 @@
-{{-- <nav class="container mx-auto" aria-label="Breadcrumb">
-    <ol class="inline-flex items-center space-x-1 rtl:space-x-reverse md:space-x-2">
-        <li class="inline-flex items-center">
-            <div class="flex items-center">
-                <svg class="mx-1 h-3 w-3 text-gray-400 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 9 4-4-4-4" />
-                </svg>
-                <a class="text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white md:ms-2"
-                    href="#">Projects</a>
-            </div>
-        </li>
-        <li>
-            <div class="flex items-center">
-                <svg class="mx-1 h-3 w-3 text-gray-400 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 9 4-4-4-4" />
-                </svg>
-                <a class="text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white md:ms-2"
-                    href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a>
-            </div>
-        </li>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <svg class="mx-1 h-3 w-3 text-gray-400 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 9 4-4-4-4" />
-                </svg>
-                <span class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 md:ms-2">Flowbite</span>
-            </div>
-        </li>
-    </ol>
-</nav> --}}
 {{-- resources/views/partials/breadcrumbs.blade.php --}}
 <nav class="flex mr-auto" aria-label="Breadcrumb">
 @unless ($breadcrumbs->isEmpty())
